036dd0ee94c629315629cfc07b7d9fa65d795538
ArcSid <raimundas.naujikas@gmail.com>
2024-09-15 19:20:32 +0300

    Initial commit

---
+++ src/main/java/org/arcsid/dungeonizercore/DungeonizerCore.java
@@ -0,0 +1,17 @@
+package org.arcsid.dungeonizercore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DungeonizerCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
